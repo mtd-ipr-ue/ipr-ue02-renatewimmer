b9c6f65e0930347b179e83172e8440d528dd7e4b
Renée Wimmer <renate.wimmer02@gmail.com>
2024-11-09 08:59:59 +0100

    Update ue02_3.java

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -8,12 +8,16 @@ public class ue02_3 {
         Out.println("Enter the y-coordiante");
         double y = In.readDouble();
 
-        double centerX = 0;
-        double radius = 4;
-        double centerY = 0;
+        double r = (Math.sqrt(x * x) + (y * y));
 
-        if ((0 <= x) && (x <= 4) && (0 <= y) && (y <= 4)) {
-            Out.println("the point is not within the green space");
-        }
+        boolean greenSpace = (0 <= x) && (x <= 4) && (0 <= y) && (y <= 4);
+        boolean outsideCircle = r > 4;
+            
+
+            if (greenSpace && outsideCircle) {
+                System.out.println("The Point is within the green space");
+           } else{
+                System.out.println("The Point is not within the green space");     
+            } 
     }
 }
